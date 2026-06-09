f57f011d6e9982a3c9bd7fa3e95e37780ba723ce
Harsh Shukla <harshnitj32@gmail.com>
2026-06-10 00:23:02 +0530

    Changes  Vite Config

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    host: true,
-    cors: true,
-    allowedHosts: [
-      '.trycloudflare.com',
-    ],
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-      },
-    },
-  },
   build: {
     outDir: 'dist',
+    
   },
+   base: "/"
 })
